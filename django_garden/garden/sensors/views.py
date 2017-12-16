0454caba0642681865f363437eefea28f7224d7b
Cristian Samaniego <chris.rubiano42@gmail.com>
2017-12-15 17:37:36 -0700

    added temporary view for temperature :ocean:

--- django_garden/garden/sensors/views.py
+++ django_garden/garden/sensors/views.py
@@ -1,3 +1,18 @@
+import serial
 from django.shortcuts import render
 
-# Create your views here.
+
+def temp_view(request):
+    context_dict = {}
+    ser = serial.Serial('/dev/ttyUSB0')
+    ser.write(b's\n')
+    while True:
+        if (ser.inWaiting()>0):
+            data = ser.readline()
+            data = data.decode("ASCII")
+            data = data.rstrip()
+            ser.close()
+            break
+    context_dict['temp'] = data
+    return render(request, 'sensors/temp.html', context_dict)
+    
