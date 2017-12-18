2054d79bf45c7907a9c9b0ce6eb67cbfe60a43c3
Cristian Samaniego <chris.rubiano42@gmail.com>
2017-12-18 14:34:33 -0700

    added a debugging led :ocean:

--- django_garden/garden/sensors/views.py
+++ django_garden/garden/sensors/views.py
@@ -1,14 +1,18 @@
 import serial
 from django.shortcuts import render
+from gpiozero import OutputDevice
 
 
 def all_sensors_view(request):
+    led = OutputDevice(17)
     context_dict = {}
     ser = serial.Serial('/dev/ttyUSB0')
+    led.on()
     ser.write(b's\n')
     while True:
         if (ser.in_waiting>0):
             data = ser.readline()
+            led.off()
             data = data.decode("ASCII")
             data = data.rstrip()
             ser.close()
