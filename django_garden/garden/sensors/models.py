3e8418b7556ef36d3a4297f918eb0aea51c5171f
Cristian Samaniego <chris.rubiano42@gmail.com>
2017-12-21 16:04:38 -0700

    added a model and a template to register and view sensor data :surfer:

--- django_garden/garden/sensors/models.py
+++ django_garden/garden/sensors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Temperature(models.Model):
+    """Model definition for Temperature."""
+
+    date = models.DateTimeField(, auto_now=False, auto_now_add=False)
+    temperature = models.FloatField()
+
+    class Meta:
+        verbose_name = 'Temperature'
+        verbose_name_plural = 'Temperatures'
+
+    def __str__(self):
+        return str(self.date)
+
