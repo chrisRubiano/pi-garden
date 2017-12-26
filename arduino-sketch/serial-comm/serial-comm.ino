cac55298bb846150e995cac317cee4561de877b9
Cristian Samaniego <chris.rubiano42@gmail.com>
2017-12-26 11:43:30 -0700

    moved arduino file :pineapple:

--- arduino-sketch/serial-comm/serial-comm.ino
+++ arduino-sketch/serial-comm/serial-comm.ino
@@ -1,22 +1,8 @@
+#include <DHT.h>
 /*
-  Serial Event example
-
-  When new serial data arrives, this sketch adds it to a String.
-  When a newline is received, the loop prints the string and clears it.
-
-  A good test for this is to try it with a GPS receiver that sends out
-  NMEA 0183 sentences.
-
-  NOTE: The serialEvent() feature is not available on the Leonardo, Micro, or
-  other ATmega32U4 based boards.
-
-  created 9 May 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/SerialEvent
+  HACE FALTA PONER UNA EXPLICACION AQUI JEJE
 */
+
 
 String inputString = "";         // a String to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
